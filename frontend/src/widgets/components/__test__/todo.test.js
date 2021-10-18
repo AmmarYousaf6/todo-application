ef1b319974ba8b6b6506b5030c1cac674c9c86fc
AmmarYousaf6 <ammaryousaf6@gmail.com>
2021-10-19 01:52:42 +0500

    Initial changes to the frontend- Test written to check if the component can render correctly without crashing

---
+++ frontend/src/widgets/components/__test__/todo.test.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDom from "react-dom";
+import { rest } from "msw";
+import { setupServer } from "msw/node";
+import { isTSAnyKeyword } from "@babel/types";
+import { render, fireEvent, waitFor, screen } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+
+import TodoInput from "../TodoInput";
+
+//check if the component renders correctly
+it("it should render Todo without crashing", () => {
+    const div = document.createElement("div");
+    ReactDom.render(<Todo></Todo>, div);
+  });
+  
