b12cec161d8bd5f4a37581f6ff9ecf35d87d07a3
AmmarYousaf6 <ammaryousaf6@gmail.com>
2021-10-19 15:30:13 +0500

    Test to check if the text buy some milk exists or not

--- frontend/src/widgets/components/__test__/todo.test.js
+++ frontend/src/widgets/components/__test__/todo.test.js
@@ -5,12 +5,16 @@ import { setupServer } from "msw/node";
 import { isTSAnyKeyword } from "@babel/types";
 import { render, fireEvent, waitFor, screen } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
-
 import TodoInput from "../TodoInput";
 
 //check if the component renders correctly
 it("it should render Todo without crashing", () => {
-    const div = document.createElement("div");
-    ReactDom.render(<Todo></Todo>, div);
-  });
+  const div = document.createElement("div");
+  ReactDom.render(<Todo></Todo>, div);
+});
+
+it("it should check if buy some milk exists or not", () => {
+  const { getByText } = render(<TodoItem todo={[{ message: "ABC" }]} />);
+  expect(getByText("Buys some milk")).toBeInTheDocument();
+});
   
