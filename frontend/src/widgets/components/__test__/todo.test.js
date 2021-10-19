39376b003befd11020cdb99d8841d4982097457f
AmmarYousaf6 <ammaryousaf6@gmail.com>
2021-10-19 15:30:44 +0500

    Test to check if the component todoInput exists or not

--- frontend/src/widgets/components/__test__/todo.test.js
+++ frontend/src/widgets/components/__test__/todo.test.js
@@ -17,4 +17,10 @@ it("it should check if buy some milk exists or not", () => {
   const { getByText } = render(<TodoItem todo={[{ message: "ABC" }]} />);
   expect(getByText("Buys some milk")).toBeInTheDocument();
 });
+
+//check if the TodoItem component renders correctly
+it("it should render TodoInput without crashing", () => {
+  const div = document.createElement("div");
+  ReactDom.render(<TodoInput></TodoInput>, div);
+});
   
